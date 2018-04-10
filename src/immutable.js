8ddc3e2ae082ec1bb078cae2866f3055bc15354f
jakobivarsson <jakob.ivarsson94@gmail.com>
2018-04-10 11:45:23 +0200

    add tests for immutable functions

--- src/immutable.js
+++ src/immutable.js
@@ -6,6 +6,9 @@ export const add = (obj, k, v) => ({
 });
 
 export const remove = (obj, k) => {
+  if (!obj[k]) {
+    return obj;
+  }
   // eslint-disable-next-line no-unused-vars
   const { [k]: omit, ...s } = obj;
   return s;
---
+++ test/immutable.test.js
@@ -0,0 +1,19 @@
+import { add, remove } from "../src/immutable";
+
+describe("immutable helper functions", () => {
+  test("add", () => {
+    const a = {};
+    const b = add(a, "a", 1);
+    expect(b).not.toBe(a);
+    expect(b).toEqual({ a: 1 });
+  });
+
+  test("remove", () => {
+    const a = { a: 1, b: 2 };
+    const b = remove(a, "c");
+    expect(b).toBe(a);
+    const c = remove(a, "b");
+    expect(c).not.toBe(a);
+    expect(c).toEqual({ a: 1 });
+  });
+});
